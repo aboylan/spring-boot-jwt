8eca496ed991866cad089b624d531c14f37fdd64
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-19 21:22:42 -0600

    Creando proyecto JWT y configuraciones

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
@@ -27,33 +28,6 @@ public class SpringSecurityConfig {
 	@Autowired
     private JpaUserDetailsService userDetailService;
 
-	/*@Bean
-	UserDetailsService userDetailsService() throws Exception {
-
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-
-		manager.createUser(
-				User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("ADMIN", "USER").build());
-
-		manager.createUser(User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build());
-
-		return manager;
-	}*/
-
-	/*@Bean
-	AuthenticationManager authManager(HttpSecurity http) throws Exception {
-		
-		AuthenticationManagerBuilder build = http.getSharedObject(AuthenticationManagerBuilder.class);
-		
-		build.jdbcAuthentication()
-			.dataSource(dataSource)
-			.passwordEncoder(passwordEncoder)
-			.usersByUsernameQuery("select username, password, enabled from users where username=?")
-			.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
-		
-		return build.build();
-	}*/
-
 	@Autowired
 	public void userDetailsService(AuthenticationManagerBuilder build) throws Exception {
 		build.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
@@ -72,16 +46,13 @@ public class SpringSecurityConfig {
 						mvc.pattern("/images/**"), mvc.pattern("/listar**"), mvc.pattern("/locale"),
 						mvc.pattern("/api/clientes/**"))
 				.permitAll()
-				/* .requestMatchers(mvc.pattern("/ver/**")).hasAnyRole("USER") */
-				/* .requestMatchers(mvc.pattern("/uploads/**")).hasAnyRole("USER") */
-				/* .requestMatchers(mvc.pattern("/form/**")).hasAnyRole("ADMIN") */
-				/* .requestMatchers(mvc.pattern("/eliminar/**")).hasAnyRole("ADMIN") */
-				/* .requestMatchers(mvc.pattern("/factura/**")).hasAnyRole("ADMIN") */
 				.anyRequest().authenticated());
 
 		http.formLogin(form -> form.successHandler(successHandler).loginPage("/login").permitAll());
 		http.logout(logout -> logout.permitAll());
 		http.exceptionHandling(sec -> sec.accessDeniedPage("/error_403"));
+		http.csrf(csrf -> csrf.disable());
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return http.build();
 	}
