87c5241039a61549ea4d0dc5b4326900ba83225e
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-23 09:42:32 -0600

    Creando una segunda clase filtro JWTAuthorizationFilter (2)

---
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,38 @@
+package com.bolsadeideas.springboot.app.auth.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String header = request.getHeader("Authorization");
+
+		if (!requiresAuthentication(header)) {
+			chain.doFilter(request, response);
+			return;
+		}
+	}
+
+	private boolean requiresAuthentication(String header) {
+		if (header == null || !header.startsWith("Bearer ")) {
+			return false;
+		}
+		return true;
+	}
+
+}
